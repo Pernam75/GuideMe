92a5a08c028fd704dc7225a5b71265a87a0883a5
Joël <joelgabbai@gmail.com>
2021-11-19 10:37:53 +0100

    old version

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,43 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Accuracy } from 'expo-location';
 import * as Location from 'expo-location';
 
 
-
 class Map extends React.Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  async getLocationAsync() {
-    const status  = Location.requestForegroundPermissionsAsync()
-    if (status.toString() === 'granted') {
-     const location = await Location.getCurrentPositionAsync({});
-    }
-  }
-
-  componentDidMount() {
-    Location.requestForegroundPermissionsAsync()
-    async function testfunction(){
-      const initialposition = await Location.getCurrentPositionAsync({ accuracy: Accuracy.Low })
-      let lat = initialposition.coords.latitude
-      let long = initialposition.coords.longitude
-    }
-  }
-
-
   render() {
-    return (
+    Location.requestForegroundPermissionsAsync()
+    let position =  Location.getCurrentPositionAsync({ accuracy: Accuracy.Low })
+    return ( 
       <MapView
          style={{ width: "100%", height: "80%" }}
          provider={PROVIDER_GOOGLE}
          showsUserLocation
          initialRegion={{
-         latitude: lat,
-         longitude: long,
+         latitude: 37.78825,
+         longitude: -122.4324,
          latitudeDelta: 0.0922,
          longitudeDelta: 0.0421}}
       />
