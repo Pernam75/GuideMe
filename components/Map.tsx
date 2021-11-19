a90853a462bd382ffde69646e6866b0775c74c3b
Joël <joelgabbai@gmail.com>
2021-11-19 09:24:44 +0100

    add constructor

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Accuracy } from 'expo-location';
 import * as Location from 'expo-location';
 
 
+
 class Map extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+  }
+
+  async getLocationAsync() {
+    const status  = Location.requestForegroundPermissionsAsync()
+    if (status === 'granted') {
+     const location = await Location.getCurrentPositionAsync({});
+    }
+  }
+
+  componentDidMount() {
     Location.requestForegroundPermissionsAsync()
-    let position =  Location.getCurrentPositionAsync({ accuracy: Accuracy.Low })
+    const initialposition = await Location.getCurrentPositionAsync({ accuracy: Accuracy.Low })
+    var lat = initialposition.coords.latitude
+    var long = initialposition.coords.longitude
+  }
+
+
+  render() {
     return (
       <MapView
          style={{ width: "100%", height: "80%" }}
          provider={PROVIDER_GOOGLE}
          showsUserLocation
          initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
+         latitude: lat,
+         longitude: long,
          latitudeDelta: 0.0922,
          longitudeDelta: 0.0421}}
       />
