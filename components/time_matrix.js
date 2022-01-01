923afdadeac4bc1e52967594a62eedea0ec1a4de
Jules <jules.rubin@efrei.net>
2022-01-01 19:24:05 +0100

    poubelle

--- components/time_matrix.js
+++ components/time_matrix.js
@@ -1,9 +1,44 @@
 const math = require('mathjs');
 const XMLHttpRequest = require('xhr2');
-
 const adress = require('./monuments.json');
 
-export function getMonuments() {
+getMonumentsOrder(2.3556456565856934, 48.787715911865234, 10000, 'durations');
+
+
+function getMonumentsOrder(positionLat, positionLong, radius, choice) {
+  
+  const selectedMonuments = monumentsInRadius(positionLat, positionLong, radius);
+  let locationsArray = [[positionLong, positionLat]]
+  selectedMonuments.forEach((value, key) => {
+    locationsArray.push([value.Longitude, value.Latitude])
+  });
+  let body = '{"locations":['
+  locationsArray.forEach(location => {
+    body += '['+location[0].toString()+', '+location[1].toString()+']';
+  });
+  body += '],"metrics":["distance","duration"]}'
+  console.log(body)
+  let requestMatrix = new XMLHttpRequest();
+  requestMatrix.open('POST', "https://api.openrouteservice.org/v2/matrix/foot-walking");
+  requestMatrix.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
+  requestMatrix.setRequestHeader('Content-Type', 'application/json');
+  requestMatrix.setRequestHeader('Authorization', '5b3ce3597851110001cf62488e507e8f47604f66ae8ba7a411f9f8bd');
+  let shorterPath = [];
+  requestMatrix.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      let array = JSON.parse(this.responseText);
+      //console.log(array)
+      //shorterPath = findShorterPath(array[choice]);
+    }
+  };
+  requestMatrix.send(body);
+
+
+  //console.log(shorterPath)
+}
+
+
+function getMonuments() {
   const allMonumentsMap = new Map();
   /*
   for (let i = 0; i < adress.length; i++) {
@@ -44,24 +79,7 @@ async function getLocation(name) {
 
 
 
-let requestMatrix = new XMLHttpRequest();
 
-requestMatrix.open('POST', "https://api.openrouteservice.org/v2/matrix/foot-walking");
-
-requestMatrix.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
-requestMatrix.setRequestHeader('Content-Type', 'application/json');
-requestMatrix.setRequestHeader('Authorization', '5b3ce3597851110001cf62488e507e8f47604f66ae8ba7a411f9f8bd');
-
-requestMatrix.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    let array = JSON.parse(this.responseText);
-    //console.log(array)
-    let choice = 'durations'; // durations ou distances
-    const shorterPath = findShorterPath(array[choice]);
-  }
-};
-
-//requestMatrix.send();
 
 function findShorterPath(adjMatrix){
   let FloydWarshall = require('floyd-warshall');
@@ -69,39 +87,31 @@ function findShorterPath(adjMatrix){
   //Getting the shortest path between each point with the Floyd-Warshall algorithm
   
   //Now we're looking for the shortest path passing through all points once
-  times = [0];
-  path = [0];
-  currentPlace = 0;
-  while (path.length < distMatrix.length-1) {
+  let times = [0];
+  let path = [0];
+  let currentPlace = 0;
+  while (path.length < distMatrix.length) {
     currentPlace = path.slice(-1);
-    nextPlaceTime = getMinimum(distMatrix[currentPlace], times)[0];
-    nextPlaceIndex = getMinimum(distMatrix[currentPlace], times)[1];
-    times.push(nextPlaceTime);
-    path.push(nextPlaceIndex);
-  }
-
-  for (let i = 0; i < distMatrix.length; i++) {
-    if (nin(path, i)) {
-      path.push(i);
-      times.push(distMatrix[i, path.slice(-1)]);
-    }
-    
+    times.push(getMinimum(distMatrix[currentPlace], path)[0]);
+    path.push(getMinimum(distMatrix[currentPlace], path)[1]);
   }
   console.log("path : "+path);
   console.log("times : "+times);
-  stringPath = monumentsMap.get(0)[0].toString();
+  let stringPath = monumentsMap.get(0)[0].toString();
   path.slice(1).forEach(element => {
     stringPath += '->'+monumentsMap.get(element)[0];
   });
   console.log(stringPath);
   console.log(totalTime(times));
+
+  return [path, times, stringPath]
 }
 
-function getMinimum(vector, times) {
+function getMinimum(vector, path) {
   let min = 1000000;
   let position = 1000000;
   for (let i = 0; i < vector.length; i++) {
-    if ((vector[i]<min) && nin(times, vector[i]) && (vector[i] != 0)) {
+    if ((vector[i]<min) && nin(path, i) && (vector[i] != 0)) {
       min = vector[i];
       position = i;
     }
@@ -122,7 +132,7 @@ function totalTime(times) {
   times.forEach(time => {
     total += parseFloat(time);
   });
-  total = total*0.65;
+  total = total;
   h = math.floor(total/3600);
   min = math.floor((total%3600)/60)
   if(math.floor(total%60) >= 30){
@@ -134,7 +144,7 @@ function totalTime(times) {
 
 // code Kais
 /* VARIABLES */
-const R = 6371 ; /* Radius of earth [km] */
+
 
 function radian(delta){
     return delta * Math.PI / 180;
@@ -142,6 +152,7 @@ function radian(delta){
 
 
 function distance (lat1, long1, lat2, long2){ /* calcul de la distance avec la formule de haversine */
+    const R = 6371 ; /* Radius of earth [km] */
     const delta_lat = radian(lat2 - lat1);
     const delta_long = radian(long2 - long1);
     const a = Math.pow(Math.sin(delta_lat / 2), 2) + Math.cos(radian(lat1)) * Math.cos(radian(lat2)) * Math.pow(Math.sin(delta_long/2), 2);
@@ -150,7 +161,7 @@ function distance (lat1, long1, lat2, long2){ /* calcul de la distance avec la f
     return distance;
 }
 
-export function monumentsInRadius(positionLat, positionLong, rayon){
+function monumentsInRadius(positionLat, positionLong, rayon){
     rayon = rayon/1000;
     const monumentsMap = getMonuments();
     const liste_monuments = new Map();
