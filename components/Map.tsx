caee93415e20f88e1ac84a3a29a9d10224d509e1
Joël <joelgabbai@gmail.com>
2022-01-02 19:19:54 +0100

    create paths

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,6 +5,7 @@ import { Accuracy } from 'expo-location';
 import * as Location from 'expo-location';
 import Openrouteservice from 'openrouteservice-js';
 import {monumentsInRadius, totalTime, getMonumentsOrder} from './time_matrix';
+import { ParenthesisNodeDependencies } from 'mathjs';
 
 class Map extends React.Component<any, any, any> {
   mapRef: any;
@@ -17,6 +18,7 @@ class Map extends React.Component<any, any, any> {
       lon: 0, 
       pathMarker: [],
       path: [],
+      paths: [],
     }
   }
 
@@ -61,17 +63,17 @@ class Map extends React.Component<any, any, any> {
       
       const getPath = async() => {
         const circuit = await getMonumentsOrder(this.state.lat, this.state.lon, this.props.radius);
-        const circuitArray = JSON.parse(circuit.toString());
+        //this.computePath(this.state.lon, this.state.lat, circuit[0].Longitude, circuit[0].Latitude, 0);
         let i = 1;
-        for(i = 1; i < circuitArray.length; i++){
-          this.computePath(circuitArray['Latitude'][i-1], circuitArray['Longitude'][i-1], circuitArray['Latitude'][i], circuitArray['Longitude'][i]);
+        for(i = 1; i < circuit.length; i++){
+          this.computePath(circuit[i-1].Latitude, circuit[i-1].Longitude, circuit[i].Latitude, circuit[i].Longitude, i);
         }
       }
       getPath();
     }
   }
 
-  computePath(latStart: string, longStart: string, latEnd: string, longEnd: string) {
+  computePath(latStart: string, longStart: string, latEnd: string, longEnd: string, i : Number) {
     let orsDirections = new Openrouteservice.Directions({ api_key: "5b3ce3597851110001cf62488e507e8f47604f66ae8ba7a411f9f8bd"});
     orsDirections.calculate({
       coordinates: [[longStart, latStart], [longEnd, latEnd]],
@@ -87,8 +89,13 @@ class Map extends React.Component<any, any, any> {
           this.setState({
             path: res
                     .features[0].geometry.coordinates
-                    .map((point: any) => ({latitude: point[1], longitude: point[0]}))
+                    .map((point: any) => ({cle : i, latitude: point[1], longitude: point[0]}))
           })
+          this.state.paths.push(this.state.path);
+          /*for(let j =0; j < this.state.paths.length; j++){
+            console.log("test", this.state.paths[j][0]);
+          }
+          console.log("fin");*/
         }
       })
     .catch((err: any) => console.error(err));
@@ -109,14 +116,15 @@ class Map extends React.Component<any, any, any> {
             center={{latitude: this.state.lat, longitude: this.state.lon}}
           />
         )}
-        {this.state.path.length > 0 && (
+        {this.state.paths.map((path: any) => (
           <Polyline
+            key = {path.cle}
             strokeColor="#FF0000"
             strokeWidth={6}
             lineDashPattern={[0]}
-            coordinates={this.state.path}
+            coordinates={path}
           />
-        )}
+        ))}
         {this.state.pathMarker.map((marker: any) => (
           <Marker 
             key={marker.name}
