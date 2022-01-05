42a40acfd7bac08622860cbedaf45f35d4feb2ad
Jules <jules.rubin@efrei.net>
2022-01-05 23:23:48 +0100

    INCREDIBLE

--- components/liste.tsx
+++ components/liste.tsx
@@ -1,28 +1,61 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View, SafeAreaView, SectionList } from 'react-native';
 import { monumentsInRadius } from './time_matrix';
-import Constants from "expo-constants";
-function transformData() {
-  const mapData = monumentsInRadius(48.85684, 2.35009, 3000);
-  let returningData: [{ key: String }]
-  returningData = [{ key: "" }]
-  mapData.forEach((value, key) => {
-    returningData.push({ key: value[0].toString() });
-    console.log(value[0]);
-  });
-  return returningData;
+import {CheckBox} from 'react-native-elements';
+
+class FlatListBasics extends React.Component<any, any, any> {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  transformData = () => {
+    const mapData = monumentsInRadius(48.85684, 2.35009, 3000);
+    let returningData: [{ id: any, key: String }];
+    returningData = [{ id: 0, key: "" }];
+    let arrayData = Array.from(mapData.values());
+    for (let i = 0; i < arrayData.length; i++) {
+      returningData.push({id: i, key: arrayData[i][0].toString() });
+      
+    }
+    return returningData.slice(1);
+  }
+  
+  renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text style={styles.item}>{item.key}</Text>
+        <CheckBox
+          // iconRight
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={false}
+        />
+      </View>
+      
+    )
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Test CheckBox</Text>
+        <FlatList
+          data={this.transformData()}
+          keyExtractor={(item) => {
+            return item.id;
+          }}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
+
 }
-const FlatListBasics = () => {
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={transformData()}
-        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
-      />
-    </View>
-  );
-}
-export default FlatListBasics;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -44,3 +77,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
 });
+
+export default FlatListBasics;
