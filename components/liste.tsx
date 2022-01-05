dcd0807dec2f30e810b0efd3aced847eaa799161
Joël <joelgabbai@gmail.com>
2022-01-05 20:07:22 +0100

    monument on tabone

--- components/liste.tsx
+++ components/liste.tsx
@@ -1,62 +1,32 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, SafeAreaView, SectionList  } from 'react-native';
-
+import { FlatList, StyleSheet, Text, View, SafeAreaView, SectionList } from 'react-native';
+import { monumentsInRadius } from './time_matrix';
 import Constants from "expo-constants";
-
-function dataTransform(){
-  const DATA = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"]
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"]
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"]
-    }
-  ];
-  return DATA;
+function transformData() {
+  const mapData = monumentsInRadius(48.85684, 2.35009, 3000);
+  let returningData: [{ key: String }]
+  returningData = [{ key: "" }]
+  mapData.forEach((value, key) => {
+    returningData.push({ key: value[0].toString() });
+    console.log(value[0]);
+  });
+  return returningData;
 }
-
-
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-
-class FlatListBasics extends React.Component<any, any, any> {
-  render(){
-      return (
-          <SafeAreaView style={styles.container}>
-            <SectionList
-              sections={dataTransform()}
-              keyExtractor={(item, index) => item + index}
-              renderItem={({ item }) => <Item title={item} />}
-              renderSectionHeader={({ section: { title } }) => (
-                <Text style={styles.header}>{title}</Text>
-              )}
-            />
-        </SafeAreaView>
-        );
-  }
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={transformData()}
+        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
 }
-
-
-
 export default FlatListBasics;
-
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    paddingTop: 22
   },
   item: {
     backgroundColor: "#f9c2ff",
@@ -67,7 +37,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     width: '100%',
-    textAlign:'center',
+    textAlign: 'center',
   },
   header: {
     fontSize: 32,
