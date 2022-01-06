edb645a8a2a06c5d7f7d1ba437422f5ea80e64f5
Benjamin <benjmin@coot.fr>
2022-01-06 12:19:22 +0100

    Fix bugs

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import { Alert } from 'react-native';
 import MapView, { Circle, Polyline, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Accuracy } from 'expo-location';
 import * as Location from 'expo-location';
 import Openrouteservice from 'openrouteservice-js';
-import {monumentsInRadius, totalTime, getMonumentsOrder} from './time_matrix';
-import { ParenthesisNodeDependencies } from 'mathjs';
+import {monumentsInRadius, getMonumentsOrder} from './time_matrix';
 
 class Map extends React.Component<any, any, any> {
   mapRef: any;
@@ -15,13 +14,13 @@ class Map extends React.Component<any, any, any> {
 
     this.state = {
       lat: 0,
-      lon: 0, 
+      lon: 0,
       pathMarker: [],
-      paths: [],
+      path: [],
     }
   }
 
-  
+
   componentDidMount() {
     const initToCurrLocation = async() => {
       const {status}  = await Location.requestForegroundPermissionsAsync()
@@ -43,45 +42,24 @@ class Map extends React.Component<any, any, any> {
 
   componentDidUpdate(prevProps) {
     if (this.props.radius !== prevProps.radius) {
-      /*version propre
-      let finalmonument = monumentsInRadius(this.state.lat, this.state.lon, this.props.radius)
-      this.setState({
-        pathMarker: Array.from(finalmonument.values()).map((monument: any) => ({name: monument[0], latitude: monument[2], longitude: monument[1]}))
-      })
-      const circuit = getMonumentsOrder(this.state.lat, this.state.lon,  this.props.radius);
-      */
-      
-      //test
-      
-      //this.computePath('48.791855', '2.331433');
-
-      
       let finalmonument = monumentsInRadius(this.state.lat, this.state.lon, this.props.radius)
       this.setState({
         pathMarker: Array.from(finalmonument.values()).map((monument: any) => ({name: monument[0], latitude: monument[2], longitude: monument[1]})),
-        paths: []
+        path: []
       })
-      let getPath = async() => {
-        /*if(this.state.paths.length > 0){
-          this.state.paths.length = 0;
-        }*/
-        
-        let circuit = await getMonumentsOrder(this.state.lat, this.state.lon, this.props.radius);
-        let i = 1;
-        for(i = 1; i < circuit.length; i++){
-          console.log("\n\n\n\nitération n°"+i+"\ndans la boucle :\n"+circuit[i-1].Nom+" : [["+circuit[i-1].Latitude + ", " + circuit[i-1].Longitude +"] vers " + circuit[i].Nom + " : [" + circuit[i].Latitude + ", "+ circuit[i].Longitude+"]]");
-          this.computePath(circuit[i-1].Latitude, circuit[i-1].Longitude, circuit[i].Latitude, circuit[i].Longitude, i);
-        }
-      }
-      getPath();
+      this.getPath();
     }
   }
 
-  computePath(latStart: string, longStart: string, latEnd: string, longEnd: string, i : Number) {
-    console.log("dans la fonction :\n"+"[Start : ["+longStart + ", " + latStart + "]\nArrivée : [" + longEnd + ", " + latEnd+"]]")
+  getPath = async() => {
+    let circuit = await getMonumentsOrder(this.state.lat, this.state.lon, this.props.radius);
+    this.computePath(circuit);
+  }
+
+  computePath = (circuit: Array<any>) => {
     let orsDirections = new Openrouteservice.Directions({ api_key: "5b3ce3597851110001cf62488e507e8f47604f66ae8ba7a411f9f8bd"});
     orsDirections.calculate({
-      coordinates: [[longStart, latStart], [longEnd, latEnd]],
+      coordinates: circuit.map(point => [point.Longitude, point.Latitude]),
       profile: "foot-walking",
       preference: 'shortest',
       // extra_info: ["waytype", "steepness"],
@@ -91,18 +69,7 @@ class Map extends React.Component<any, any, any> {
     .then((res: any) => {
         // Add your own result handling here
         if (res?.features?.[0]?.geometry?.coordinates) {
-          let tmppath =
-                  res.features[0].geometry.coordinates
-                  .map((point: any) => ({latitude: point[1], longitude: point[0]}))
-          this.setState(
-            {paths: [...this.state.paths, {chemin :tmppath, cle:i}]}
-          )
-         //console.log("taille chemin :" ,this.state.paths[i].chemin.length);
-
-          /*for(let j =0; j < this.state.paths.length; j++){
-            console.log("test", this.state.paths[j][0]);
-          }
-          console.log("fin");*/
+          this.setState({path: res.features[0].geometry.coordinates.map((point: any) => ({latitude: point[1], longitude: point[0]}))})
         }
       })
     .catch((err: any) => console.error(err));
@@ -123,17 +90,16 @@ class Map extends React.Component<any, any, any> {
             center={{latitude: this.state.lat, longitude: this.state.lon}}
           />
         )}
-        {this.state.paths.map((path: any) => (
+        {this.state.path.length > 0 && (
           <Polyline
-            key = {path.cle}
             strokeColor="#FF0000"
             strokeWidth={6}
             lineDashPattern={[0]}
-            coordinates={path.chemin}
+            coordinates={this.state.path}
           />
-        ))}
+        )}
         {this.state.pathMarker.map((marker: any) => (
-          <Marker 
+          <Marker
             key={marker.name}
             coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
             title={marker.name}
