dd028b2f872f717851b3120599c887f625a54ac6
Jules <jules.rubin@efrei.net>
2022-01-13 14:36:43 +0100

    radio check

--- components/TransportRadio.tsx
+++ components/TransportRadio.tsx
@@ -1,33 +1,53 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import { RadioButton } from 'react-native-paper';
+import { RadioButton, RadioGroup } from "react-native-radio-check"
 
 class TransportRadio extends React.Component<any, any, any> {
-
+    constructor(props) {
+        super(props)
+        this.state = { check: true, index: 0 }
+    }
     render() {
-        const [checked, setChecked] = React.useState('first');
         return(
             <View>
+                <RadioGroup
+                style={{ flexDirection: 'row', marginTop: 10}}
+                checkedId={this.state.index}
+                iconStyle={{
+                }}
+                textStyle={{ marginLeft: 5 }}
+                onChecked={(id, value) => {
+                    console.info("Group===", id)
+                    console.info("Value===", value)
+                }}>
                 <RadioButton
-                    value="first"
-                    status={ checked === 'first' ? 'checked' : 'unchecked' }
-                    onPress={() => setChecked('first')}
-                />
+                    icon={{
+                        normal: require('../assets/images/icons/car-unchecked.png'),
+                        checked: require('../assets/images/icons/car-checked.png')
+                    }}
+                    value={"car"} />
                 <RadioButton
-                    value="second"
-                    status={ checked === 'second' ? 'checked' : 'unchecked' }
-                    onPress={() => setChecked('second')}
-                />
+                    icon={{
+                        normal: require('../assets/images/icons/radio-unchecked.png'),
+                        checked: require("../assets/images/icons/radio-checked.png")
+                    }}
+                    style={{ marginLeft: 20 }}
+                    value={2} />
                 <RadioButton
-                    value="third"
-                    status={ checked === 'third' ? 'checked' : 'unchecked' }
-                    onPress={() => setChecked('third')}
-                />
+                    icon={{
+                        normal: require('../assets/images/icons/radio-unchecked.png'),
+                        checked: require("../assets/images/icons/radio-checked.png")
+                    }}
+                    style={{ marginLeft: 20 }}
+                    value={3} />
                 <RadioButton
-                    value="fourth"
-                    status={ checked === 'fourth' ? 'checked' : 'unchecked' }
-                    onPress={() => setChecked('fourth')}
-                />
+                    icon={{
+                        normal: require('../assets/images/icons/radio-unchecked.png'),
+                        checked: require("../assets/images/icons/radio-checked.png")
+                    }}
+                    style={{ marginLeft: 20 }}
+                    value={4} />
+                </RadioGroup>
             </View>
         )
     }
